73780f2b401a4a1f15213a18d69a79cd65f370b9
Todd Croak-Falen <toddcf@gmail.com>
2017-02-09 23:16:17 -0800

    Object.create variation.

--- script.js
+++ script.js
@@ -64,3 +64,11 @@ var john = Object.create(personProto);
 john.name = "John";
 john.yearOfBirth = 1990;
 john.job = "teacher";
+
+var jane = Object.create(personProto,
+{
+	name: { value: "Jane" },
+	yearOfBirth: {value: 1969 },
+	job: {value: "designer" }
+});
+
