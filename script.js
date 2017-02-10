01a80bba2d1b852e57b3dd06475791fd377b59a1
Todd Croak-Falen <toddcf@gmail.com>
2017-02-09 19:32:25 -0800

    Constructed an object called john, passing in his name, year of birth, and job.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Function Constructor
 
+/*
 var john = {
 	name: "John",
 	yearOfBirth: 1990,
 	job: "teacher"
 };
+*/
 
 var Person = function(name, yearOfBirth, job) {
 	this.name = name;
 	this.yearOfBirth = yearOfBirth;
 	this.job = job;
 }
+
+var john = new Person("John", 1990, "teacher");
+console.log(john);
