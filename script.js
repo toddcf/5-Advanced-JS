8812be7782cf998029d622c6d54fc617374b27cd
Todd Croak-Falen <toddcf@gmail.com>
2017-02-19 13:19:15 -0800

    Basic game function to choose a number between 0 and 9. You 'win' if it chooses 5 or higher.

--- script.js
+++ script.js
@@ -151,7 +151,7 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(rates);
 */
 // Functions Returning Functions
-
+/*
 function interviewQuestion(job) {
 	if (job === "designer") {
 		return function(name) {
@@ -189,6 +189,27 @@ var specialForcesQuestion = interviewQuestion("special forces operator");
 specialForcesQuestion("Jack Bauer");
 
 interviewQuestion("special forces operator")("Jack Bauer");
+*/
+
+// Immediately Invoked Function Expressions (IIFE)
+
+function game() {
+	var score = Math.random () * 10;
+	console.log(score >= 5);
+}
+
+game();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
